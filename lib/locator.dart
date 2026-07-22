c072fffe39aea5d19e89ec85e46ce68c22a9af86
David Michael II <knowdavidmichael@gmail.com>
2026-07-22 12:06:15 +0100

    feat: Add InteractionManager and register in locator
    - **New Manager:** Created a skeleton `InteractionManager` class in the `_shared/manager` directory.
    - **Dependency Injection:** Registered `InteractionManager` as a singleton in `locator.dart` to make it available via the service locator.

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,6 +1,8 @@
+import 'package:fiakkere/_shared/manager/interaction_manager.dart';
 import 'package:fiakkere/_shared/services/database_service.dart';
 import 'package:flutter_it/flutter_it.dart';
 
 void configureDependencies() {
+  di.registerSingleton<InteractionManager>(InteractionManager());
   di.registerSingletonAsync<Database>(Database.create);
 }
